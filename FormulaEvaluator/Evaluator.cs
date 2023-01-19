df864dfe3b03c931bbbe0468485f8cdde5933c1c
Jiwon Park <jiwon@MacBook-Pro-63.local>
2023-01-18 18:59:34 -0700

    init

--- FormulaEvaluator/Evaluator.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FormulaEvaluator;
-public class Evaluator
-{
-
-}
-
---
+++ FormulaEvaluator/FormulaEvaluator.cs
@@ -0,0 +1,28 @@
+﻿/// <summary>
+/// Author:    Jiwon Park
+/// Partner:   None
+/// Date:      Jan 18 2023
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and Jiwon Park - This work may not 
+///            be copied for use in Academic Coursework.
+///
+/// I, Jiwon Park, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from another source.  All 
+/// references used in the completion of the assignments are cited 
+/// in my README file.
+///
+/// File Contents
+///
+///    [... and of course you should describe the contents of the 
+///    file in broad terms here ...]
+/// </summary>
+
+namespace FormulaEvaluator;
+public static class Evaluator
+{
+    static Stack<int> value = new Stack<int>();
+    static Stack<String> operator = new Stack<String>();
+
+
+}
+
