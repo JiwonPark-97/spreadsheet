217fd457652e7e7969aa1c7dfefcd785e061ad09
Jiwon Park <jiwon@MacBook-Pro-63.local>
2023-02-27 20:37:06 -0700

    added starting code. need to implement FileMenuNew and FileMenuOpenAsync to .cs file

---
+++ GUI/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿namespace GUI;
+
+public partial class MainPage : ContentPage
+{
+	private readonly char[] ROWHEADERS = "ABCDEFGHIJ".ToArray();
+	private readonly int ROWS = 50;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		InitializeGrid();
+	}
+
+    private void InitializeGrid()
+    {
+		// Upper left corner
+		TopLabels.Add(
+			new Border
+			{
+				Stroke = Color.FromRgb(0, 0, 0),
+				StrokeThickness = 1,
+				Content = new Label
+				{
+					Text = $" - ",
+					WidthRequest = 25
+				}
+			}
+			);
+
+		// Top Row Label, for upper left corner
+		foreach(var label in ROWHEADERS)
+		{
+			TopLabels.Add(
+				new Border
+				{
+					Stroke = Color.FromRgb(0, 0, 0),
+					StrokeThickness = 1,
+					HeightRequest = 20,
+					WidthRequest = 75,
+					HorizontalOptions = LayoutOptions.Center,
+					Content = new Label
+					{
+						Text = $"{label}",
+						BackgroundColor = Color.FromRgb(200, 200 , 250),
+						HorizontalTextAlignment = TextAlignment.Center
+					}
+				}
+				); 
+		}
+
+		// All Spreadsheet Grid Cells
+		for(int row = 0; row< ROWS; row++)
+		{
+			var horiz = new HorizontalStackLayout();
+			// Left Column Lables
+			horiz.Add(
+				new Border
+				{
+					Stroke = Color.FromRgb(0, 0, 0),
+                    StrokeThickness = 1,
+                    HeightRequest = 30,
+                    WidthRequest = 25,
+					Content = new Label
+					{
+						Text= $"{row + 1}",
+						VerticalTextAlignment = TextAlignment.Center,
+                        BackgroundColor = Color.FromRgb(200, 200, 250)
+                    }
+                });
+			foreach(var label in ROWHEADERS)
+			{
+				var entry = new Entry
+				{
+					Text = $"{label}{row + 1}",
+					WidthRequest = 75,
+					StyleId = $"{label}{row + 1}"
+				};
+
+				_cells.Add(entry.StyleId, entry);
+
+				entry.Completed += CellCahngedValue;
+				entry.Focused += CellFocused;
+
+				horiz.Add(entry);
+			}
+
+			Grid.Children.Add(horiz);
+		}
+    }
+}
+
+
---
+++ GUI/README.md
@@ -0,0 +1,28 @@
+﻿
+```
+Author:     Jiwon Park
+Partner:    None
+Course:     CS 3500, University of Utah, School of Computing
+GitHub ID:  JiwonPark-97
+Repo:       https://github.com/uofu-cs3500-spring23/spreadsheet-JiwonPark-97.git
+Date:       3-Mar-2023 11.30 am (when submission was completed) 
+Project:    Spreadsheet
+Copyright:  CS 3500 and Jiwon Park - This work may not be copied for use in Academic Coursework.
+```
+
+# Comments to Evaluators:
+
+
+# Assignment Specific Topics
+
+    This section was not required.
+
+# Consulted Peers:
+
+    I have not directly consulted with others but referred to the Discord server listed below in reference.
+
+# References:
+
+    1. CS 3500 Spring: Software Practice - https://discord.gg/psqhddzf
+    2.
+    3. 
